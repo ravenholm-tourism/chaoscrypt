3ffa6cf5a54998d1582e5504f265efd34f413d21
michiel shortt <michielshortt@gmail.com>
2020-07-05 16:19:25 -0400

    updated plaintext class for decryption

--- src/plaintext.py
+++ src/plaintext.py
@@ -1,41 +1,35 @@
-import numpy as np
-import math
-# import string
 from chaosmaps import logisticmap
 
 class PlainText:
   ivl = [.2, .8]
-
-  def __init__ (self, charset = (), msg = ""):
-    self.set_message (charset, msg)
   
-  def __init__ (self, keystream = None, key = None, charset = ()):
-    ''' private key is initial x val, control parameter, and charset mapping '''
-    self.keystream = keystream
+  def __init__ (self, key = {}):
+    '''
+    key is dict with initial x val, control parameter,
+    and charset mapping (dict of chars:intervals)
+    '''
     self.key = key
-    self.charset = charset
+    self.charset = [*key.keys()]
   
-  def set_message (self, charset = (), msg = ""):
-    self.charset = charset
-    self.msg = msg
-  
-  def decrypt_ciphertext (self):
+  def decrypt_ciphertext (self, ciphertext):
+    '''
+    ciphertext is a list of integers indicating how many times to iterate to 
+    get the interval corresponding to the plaintext char
+    '''
     x = self.key["x_init"]
     r = self.key["ctrl_param"]
     rev_charmap = dict(map(reversed, self.key["charmap"].items()))
     rev_charmap = sorted(rev_charmap.items())
-    eps = (self.ivl[1] - self.ivl[0]) / np.float64(len(self.charset))
-    # print eps
     self.msg = ""
 
-    for c in self.keystream.ciphertext:
+    for c in ciphertext:
       for i in range(c):
         x = logisticmap(r, x)
-      # print x
-      for i in range(len(rev_charmap)):
-        if rev_charmap[i][0][0] <= x and rev_charmap[i][0][1] > x:
-          self.msg += rev_charmap[i+1][1]
-          # print self.msg
+      print(x)
+      for i in rev_charmap:
+        # check that x is in interval
+        if i[0][0] <= x and i[0][1] > x:
+          self.msg += i[1]   # add char mapped to interval to msg
           break
 
     return self.msg
